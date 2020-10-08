def3c9852bdf8d03c9dbd90600a61d365bafa75c
Samuel A Parker <samuelaparker@samuels-mbp-2.lan>
2020-10-07 23:12:27 -0400

    first commit

---
+++ src/components/Search.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+const Search = () => {
+    const [term, setTerm] = useState('');
+    return (
+        <div>
+            <div className="ui form">
+                <div className="field">
+                    <label>Enter Search term</label>
+                    <input
+                        className="input"
+                        type="text"
+                        value={term}
+                        onChange={e => setTerm(e.target.value)}
+
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Search; 
