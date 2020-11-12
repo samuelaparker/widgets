905c82c1905a79841947ebc0002d3ee24dc356e1
Samuel A Parker <samuelaparker@samuels-mbp-2.lan>
2020-11-12 13:00:46 -0500

    search api

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Search = () => {
-    const [term, setTerm] = useState('');
+    const [term, setTerm] = useState('programming');
+    const [results, setResults] = useState([]);
+    useEffect(() => {
+        const search = async () => {
+            const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: term
+                }
+            })
+            setResults(data.query.search);
+        };
+        search();
+    }, [term]);
+
+    const renderedResults = results.map((results) => {
+        return <div className="item">
+            <div className="content">
+                <div className="header">
+                    {results.title}
+                </div>
+                {results.snippet}
+            </div>
+        </div>
+    })
+
     return (
         <div>
             <div className="ui form">
@@ -12,12 +41,14 @@ const Search = () => {
                         type="text"
                         value={term}
                         onChange={e => setTerm(e.target.value)}
-
                     />
                 </div>
+            </div>
+            <div className="ui celled list">
+                {renderedResults}
             </div>
         </div>
     )
 }
 
-export default Search; 
+export default Search;     
