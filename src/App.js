7502c30d91e79ed1dc857468c003893c1f130c55
Samuel A Parker <samuelaparker@samuels-mbp-2.lan>
2020-11-12 20:11:35 -0500

    11.12.20 8:11pm

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useState } from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
+import Dropdown from './components/Dropdown';
 
 const items = [
     {
@@ -17,9 +18,30 @@ const items = [
     }
 ]
 
+const options = [
+    {
+        label: 'The Color Red',
+        value: 'red'
+    },
+    {
+        label: 'The Color Green',
+        value: 'green'
+    },
+    {
+        label: 'The Color Blue',
+        value: 'Blue'
+    }
+]
+
 export default () => {
+    const [selected, setSelected] = useState(options[0]);
+
     return <div>
-        <Search />
+        <Dropdown
+            selected={selected}
+            onSelectedChange={setSelected}          
+            options={options}
+        />
     </div>
 };
 
--- src/components/Search.js
+++ src/components/Search.js
@@ -3,7 +3,19 @@ import axios from 'axios';
 
 const Search = () => {
     const [term, setTerm] = useState('programming');
+    const [debouncedTerm, setDebouncedTerm] = useState(term)
     const [results, setResults] = useState([]);
+
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setDebouncedTerm(term);
+        }, 1000);
+
+        return () => {
+            clearTimeout(timerId);
+        }
+    }, [term]); //runs anytime term changes
+
     useEffect(() => {
         const search = async () => {
             const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
@@ -12,28 +24,13 @@ const Search = () => {
                     list: 'search',
                     origin: '*',
                     format: 'json',
-                    srsearch: term
+                    srsearch: debouncedTerm,
                 }
             })
             setResults(data.query.search);
         };
-
-        if (term && !results.length) { 
-            search();
-        } else {
-            const timeoutId = setTimeout(() => {
-                if (term) {
-                    search();
-                }
-            }, 1000);
-
-            return () => {
-                clearTimeout(timeoutId);
-            };
-        }
-
-
-    }, [term, results.length]);
+        search();
+    }, [debouncedTerm]);
 
     const renderedResults = results.map((results) => {
         return <div key={results.pageid} className="item">
