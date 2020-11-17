4e6e6f7b6ebc92813bd2b7e8d004d52c6ffd2b3d
Samuel A Parker <samuel.a.parker00@gmail.com>
2020-11-16 19:59:43 -0500

    11.16.20 7:59

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState } from 'react';
+import React, { useState } from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
@@ -34,11 +34,38 @@ const options = [
     }
 ]
 
+const showAccordion = () => {
+    if (window.location.pathname === '/') {
+        return <Accordion items={items} />;
+    }
+};
+const showList = () => {
+    if (window.location.pathname === '/list') {
+        return <Search />
+    }
+};
+const showDropdown = () => {
+    if (window.location.pathname === '/dropdown') {
+        return <Dropdown />
+    }
+};
+const showTranslate = () => {
+    if (window.location.pathname === '/translate') {
+        return <Translate />
+    }
+};
+
+
+
 export default () => {
     return <div>
-            <Translate />
+        {showAccordion()}
+        {showList()}
+        {showDropdown()}
+        {showTranslate()}
     </div>
 };
+
 
 
 
